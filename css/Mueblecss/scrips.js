8e533306c91c98c74f09f2c991ed9226ebe7e321
ALEnsGAMER <62356565+alensgamer@users.noreply.github.com>
2022-03-07 14:00:10 -0600

    Commit initial

---
+++ css/Mueblecss/scrips.js
@@ -0,0 +1,17 @@
+function Aprendizaje1(){
+  document.getElementById( "vid1" ).style.display="block";
+  document.getElementById( "cer" ).style.display="block";
+  document.getElementById( "som" ).style.display="block";
+}
+function Aprendizaje2(){
+  document.getElementById( "vid2" ).style.display="block";
+  document.getElementById( "cer" ).style.display="block";
+  document.getElementById( "som" ).style.display="block";
+}
+function cerrar(){
+  document.getElementById( "vid1" ).style.display="none";
+  document.getElementById( "vid2" ).style.display="none";
+  document.getElementById( "cer" ).style.display="none";
+  document.getElementById( "som" ).style.display="none";
+  location.href=location.href;
+}
